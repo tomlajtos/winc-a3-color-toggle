cd145843d87f06c0d1fd095243d9fb35d4e203fe
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2022-12-05 14:48:58 +0100

    Update classes: BasicActions, MenuActions and comments
    Extend and rewrite comments in script2.js
    
    Update BasicActions Class:
    - add validKeys property: object
      { keypress event: "value"}
    - add isMenuVisible method:
      verifies visible/hidden state of the menu
    
    Update MenuActions Class:
      re-structure the whole class
    - add functions:
      - showHideMenuOnClick
      - showHideMenuOnHover
      - showHideMenuWithKeyboard
    - rename showHideColorMenu - showHideMenu
      - it only calls above 3 functions, no other role
      - most of its code was re written in above functions

--- script2.js
+++ script2.js
@@ -1,4 +1,9 @@
-/* Object representing elements of the DOM that are changed or interacted with
+/* This is an alternative, object-oriented solution for the color toggle assignment
+ * It is my own initiative to learn about and practice JS-OOP as it is not included in the
+ * JS curriculum of the Front-End Development course */
+
+// DEFINE A DOCUMENT OBJECT
+/* Object, representing elements of the DOM that are changed or interacted with
  * when the different functionalities are used. These properties are given as arguments
  * (in form of a de-structured object) for the BasicActions, MenuActions
  * and ColorActions classes and are used by the methods defined within these classes. */
@@ -16,10 +21,11 @@ const doc = {
   colorIndicator: document.getElementById("color-indicator"), // color indicator in footer
 };
 
-/* This class holds methods shared with and inherited by
- * MenuActions and ColorActions classses
- * constructor function takes an object with required
- * property - value pairs as an argument */
+// DEFINE CLASSES
+/* BasicActions Class holds methods shared with and inherited by
+ * MenuActions and ColorActions classes
+ * It's constructor function takes an object with required
+ * property - value pairs as an argument - 'doc' */
 class BasicActions {
   constructor({ body, colorIndicator, colorText, menuItems: m }) {
     this.menu = m.colorMenu;
@@ -27,9 +33,23 @@ class BasicActions {
     this.body = body;
     this.colorIndicator = colorIndicator;
     this.colorText = colorText;
+    this.validKeys = {
+      Enter: "open_close",
+      1: "default",
+      2: "violet",
+      3: "blue",
+      4: "cyan",
+      5: "green",
+      6: "yellow",
+      7: "orange",
+      8: "red",
+    };
   }
 
-  // shows color menu when mouse hovers over the menu button
+  /* Helper function to other functions which show color menu when:
+   * - the mouse hovers over the menu button
+   * - when mouse clicks on the menu button
+   * - when user hits the 'Enter' key */
   showColorMenu() {
     this.menu.style.opacity = "1";
     this.menu.style.left = "3px";
@@ -37,8 +57,12 @@ class BasicActions {
       "left 500ms ease-in-out 300ms, opacity 500ms ease-out 100ms";
   }
 
-  // hides color menu when mouse leaves the menu button or the color menu
-  // without selecting a new color
+  /* Helper function to other functions which hide the color menu when:
+   * - the mouse hovers off the menu button, but not towards menu
+   * - the mouse hovers off the menu without selecting a new color
+   * - when mouse clicks on the menu button the 2nd time
+   * - when user clicks on a color selector element
+   * - when user hits the 'Enter' key the 2nd time */
   hideColorMenu() {
     this.menu.style.opacity = "0";
     this.menu.style.left = "-220px";
@@ -46,10 +70,21 @@ class BasicActions {
       "left 500ms ease-in-out 400ms, opacity 500ms ease-out 600ms";
   }
 
-  // resets certain changes on specific elements between two color change event
-  // removes every class from body and colorIndicator
-  // removes "selected" class from colorSelector elements
-  // this should be called as first step on a color change event
+  /* Helper function to verify if the menu is visible or hidden
+   * Supports other functions which execution depends on the menu's hidden/visible state */
+  isMenuVisible() {
+    // reg-ex to check if menu style position-left starts with a digit == visible
+    // hidden means negative pixel value, which starts with '-'
+    const menuStateRegEx = /^\d+/g;
+    // test regex on menu element style value
+    return menuStateRegEx.test(this.menu.style.left);
+  }
+
+  /* Helper function to resets certain changes on specific elements
+   * between two color change event
+   * Removes every class from body and colorIndicator
+   * Removes "selected" class from colorSelector elements
+   * This function should be called as first step on a color change event */
   resetChanges() {
     this.body.classList.remove(...this.body.classList);
     this.colorIndicator.classList.remove(...this.colorIndicator.classList);
@@ -59,10 +94,10 @@ class BasicActions {
   }
 }
 
-/* MenuActions class provides menu operation functionalities
- * this is implemented by showHideColormenu method which calls
+/* MenuActions Class provides menu operation functionalities
+ * This is implemented by showHideColormenu method which calls
  * methods inherited from BasicActions class
- * Argument for constructor: 'doc' object */
+ * Argument for it's constructor: 'doc' object */
 class MenuActions extends BasicActions {
   constructor({ header, menuItems: m }) {
     super({ menuItems: m });
@@ -71,19 +106,84 @@ class MenuActions extends BasicActions {
     this.buttonEdge = m.buttonEdge;
     this.menuEdge = m.colorMenuEdge;
   }
-  showHideColorMenu = function() {
+
+  /* Function that adds the functionality to show/hide menu with keyboard.
+   * Show menu when 'Enter' is pressed, or hide it if it was visible
+   * Alert if key is invalid */
+  showHideMenuWithKeyboard() {
+    document.addEventListener("keypress", (event) => {
+      const keyName = event.key;
+      const validKey = keyName in this.validKeys;
+
+      // check if a key with associated function was pressed, alert if not
+      if (!validKey) {
+        alert(
+          `Key not recognized.
+        - Open / Close color menu:
+            Enter
+        - Color options (type the number to apply color):
+            default: 1,  violet: 2
+            blue: 3,  cyan: 4
+            green: 5,  yellow: 6
+            orange: 7,  red: 8`
+        );
+        // if key is valid, check if it was 'Enter'
+      } else if (
+        validKey &&
+        // if key is 'Enter', open/close menu depending on state
+        this.validKeys[keyName] === "open_close" &&
+        !this.isMenuVisible()
+      ) {
+        this.showColorMenu();
+      } else {
+        this.hideColorMenu();
+      }
+    });
+  }
+
+  /* Function that adds the functionality to show/hide menu on mouse click.
+   * Show menu when user clicks on the menu button or hide it if it was visible */
+  showHideMenuOnClick() {
+    this.button.addEventListener("click", () => {
+      !this.isMenuVisible() ? this.showColorMenu() : this.hideColorMenu();
+    });
+  }
+
+  /* Function that adds the functionality to show/hide menu
+   * depending on the cursor's hover position.
+   * Show menu when the cursor hover over the menu button
+   * Hide menu when the cursor hovers off the menu button, but not towards menu
+   * Hide menu when the cursor hovers off the menu
+   * without the user selecting a new color */
+  showHideMenuOnHover() {
     // show menu when mouse hovers on menu button
-    this.button.addEventListener("mouseover", () => this.showColorMenu());
-    // hide c. menu when mouse moves from m. button(edge) to header element
-    // but keep c. menu open when mouse moves from m.button to c. menu
+    this.button.addEventListener("mouseover", () => {
+      this.showColorMenu();
+    });
+
+    // hide menu when mouse moves from m. button(edge) to header element
+    // but keep menu open when mouse moves from m.button to menu
     this.buttonEdge.addEventListener("mouseleave", () => {
       this.buttonParent.addEventListener(
         "mouseover",
-        () => this.hideColorMenu(),
+        () => {
+          this.hideColorMenu();
+        },
         { once: true }
       );
     });
-    // hide c. menu when mouse leaves c. menu(edge) without color selection
-    this.menuEdge.addEventListener("mouseleave", () => this.hideColorMenu());
+
+    // hide menu when mouse leaves menu(edge) without color selection
+    this.menuEdge.addEventListener("mouseleave", () => {
+      this.hideColorMenu();
+    });
+  }
+
+  // Function that combines all menu actions together
+  // show/hides menu on hover, click, or keyboard event
+  showHideMenu = function() {
+    this.showHideMenuWithKeyboard();
+    this.showHideMenuOnClick();
+    this.showHideMenuOnHover();
   };
 }
